9d3f85c93ea7f475ee155c398dc74e8ffe9f5e7c
wumingke <766190553@qq.com>
2019-11-22 17:40:52 +0800

    首页appBar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List _imageUrls = [
+    "http://up.deskcity.org/pic/9f/b0/d7/9fb0d7779107e15af7c30890e12967f1.jpg",
+    "http://img06file.tooopen.com/images/20171224/tooopen_sy_231021357463.jpg",
+    "http://p2-q.mafengwo.net/s12/M00/70/71/wKgED1wTjPeAZH5EABE_sZd4tAc42.jpeg?imageMogr2%2Fthumbnail%2F%21690x370r%2Fgravity%2FCenter%2Fcrop%2F%21690x370%2Fquality%2F100",
+  ];
+
+  double _appBarAlpha = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (notification) {
+                if (notification is ScrollUpdateNotification &&
+                    notification.depth == 0) {
+                  _onScroll(notification.metrics.pixels);
+                }
+                return true;
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text("haha"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text("首页"),
+                ),
+              ),
+            ),
+            opacity: _appBarAlpha,
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onScroll(double pixels) {
+    double alpha = pixels / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(
+      () {
+        _appBarAlpha = alpha;
+      },
+    );
+  }
+}
